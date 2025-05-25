a099d852ec102c7bbad437c0686be623b7bc90c2
yusufozben <yusufozben@gmail.com>
2025-05-26 02:46:36 +0300

    syntax error generation

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 
 class HealthResponse(BaseModel):
-    status: str  # TODO: 1- pylint syntax error. delete tab
+status: str  # TODO: 1- pylint syntax error. delete tab
 
 
 # TODO: 2- add blank lines for formatting error
